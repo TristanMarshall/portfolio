820dda7e20629e8c7e496314f1d765430d1b6244
TristanMarshall <tristanmarshall@ymail.com>
2017-03-29 13:39:18 -0500

    merging changes

---
+++ build/unbundled/bower_components/polymerfire/demo/firebase-messaging-sw.js
@@ -0,0 +1,1 @@
+var window=self;importScripts("../../firebase/firebase-app.js","../../firebase/firebase-messaging.js"),firebase.initializeApp({messagingSenderId:"153517668099"});var messaging=firebase.messaging();
